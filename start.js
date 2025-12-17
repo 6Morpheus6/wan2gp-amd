0fd8b42d3f39d8cfdeb675a8e8ac8ce1a48975d8
6Morpheus6 <wanton-decay@gmx.net>
2025-12-17 20:45:09 +0100

    update
    deactivate blue screen

--- start.js
+++ start.js
@@ -13,6 +13,12 @@ module.exports = {
         on: [{
           "event": "/http:\/\/\\S+/",
           "done": true
+        }, {
+          "event": "/errno/i",
+          "break": false
+        }, {
+          "event": "/error:/i",
+          "break": false
         }]
       }
     },
