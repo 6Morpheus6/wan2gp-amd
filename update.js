767439a3d2e9576ac391bfbd7233a9ab07d14127
6Morpheus6 <wanton-decay@gmx.net>
2026-04-14 04:03:01 +0200

    update
    - ensure git pull won't fail because any2video.py has changed

--- update.js
+++ update.js
@@ -5,10 +5,23 @@ module.exports = {
       message: "git pull"
     }
   }, {
+    method: "fs.rm",
+    params: {
+      path: "app/models/wan/any2video.py",
+    }
+  }, {
     method: "shell.run",
     params: {
       path: "app",
       message: "git pull"
     }
-  }]
+  }, {
+      method: "shell.run",
+      params: {
+        path: "app",
+        message: [
+          "powershell -Command \"(Get-Content models/wan/any2video.py) -replace 'from .distributed.fsdp import shard_model', '# from .distributed.fsdp import shard_model' | Set-Content models/wan/any2video.py\""
+        ]
+      }
+    }]
 }
