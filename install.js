eb27b0e3a28cf8f4e738f6165b1de99a23e158a6
6Morpheus6 <wanton-decay@gmx.net>
2026-04-29 00:13:03 +0200

    remove patch (obsolete)

--- install.js
+++ install.js
@@ -7,13 +7,6 @@ module.exports = {
       }
     },
     {
-      method: "shell.run",
-      params: {
-        path: "app",
-        message: "powershell -Command \"(Get-Content models/wan/any2video.py) -replace 'from .distributed.fsdp import shard_model', '# from .distributed.fsdp import shard_model' | Set-Content models/wan/any2video.py\""
-      }
-    },
-    {
       method: "script.start",
       params: {
         uri: "torch.js",
